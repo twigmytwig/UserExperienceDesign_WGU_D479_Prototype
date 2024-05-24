ead0200b3f44135918574374037e09743732caed
Trevor Hedgpeth <trevorthebeast1217@gmail.com>
2024-05-24 06:18:18 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UserExperienceDesign_WGU_D479_Prototype.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
